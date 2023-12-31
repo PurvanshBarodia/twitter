3bb5d2376b265efd60628924cdbb32239c21a8ff
PurvanshBarodia <purvanshbarodia09@gmail.com>
2023-12-31 20:41:16 +0530

    cofing file added

--- database.py
+++ database.py
@@ -1,7 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from configparser import ConfigParser
 
-URL_DATABASE = 'mysql+pymysql://root:1234@localhost:3306/new'
+config = ConfigParser()
+config.read('config.ini')
+
+database_config = config['database']
+
+# Constructing the URL_DATABASE using values from the configuration file
+URL_DATABASE = f"mysql+pymysql://{database_config['username']}:{database_config['password']}@{database_config['host']}:{database_config['port']}/{database_config['database_name']}"
+
+#URL_DATABASE = 'mysql+pymysql://root:1234@localhost:3306/new'
 engine = create_engine(URL_DATABASE)
 
 SessionLocal = sessionmaker(bind=engine)
